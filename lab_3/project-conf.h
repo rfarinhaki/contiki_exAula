5081ed39dec2600384a0196645adaf33740a3ea1
Ricardo Farinhaki <ricardofarinhaki@gmail.com>
2018-07-20 21:21:48 -0300

    aula 4 lab 3

---
+++ lab_3/project-conf.h
@@ -0,0 +1,14 @@
+/*
+ * project-conf.h
+ *
+ *  Created on: Jul 6, 2018
+ *      Author: mint
+ */
+
+#ifndef PROJECT_CONF
+#define PROJECT_CONF
+
+#define BUTTON_SENSOR_CONF_ENABLE_SHUTDOWN 0
+#define ROM_BOOTLOADER_ENABLE 1
+
+#endif /* PROJECT_CONF_H_ */
